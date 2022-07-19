540050f4b2dcede725a946cb0dcead5f2b40cd81
DaiyanRK <robinkoester98@aol.com>
2022-07-19 12:01:16 +0200

    Added port

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.post("/", (req, res) => {
   res.render("finished", { gameResult: gameState });
 });
 
-app.listen("3000", () => {
+app.listen("3000" || process.env.PORT, () => {
   console.log("Server started on port 3000");
 });
