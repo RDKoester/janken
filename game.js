1e112dc7ad7a5e3e12368370212b65a6d1c593e2
DaiyanRK <robinkoester98@aol.com>
2022-07-20 11:46:35 +0200

    More code refactoring

--- game.js
+++ game.js
@@ -2,34 +2,51 @@
 
 exports.result = function (playerPick) {
 
+  let computerPick = "";
+  let result = "";
+  const win = "You win!!"
+  const lose = "You lose!!"
+  const rock = "Rock"
+  const paper = "Paper"
+  const scissors = "Scissors"
+
   // Generating a random pick for the computer
   const randomPick = Math.ceil(Math.random() * 3);
-  let computerPick = "";
-
+  
   if (randomPick === 1) {
-    computerPick = "Rock";
-  } else if (randomPick === 2) {
-    computerPick = "Paper";
-  } else if (randomPick === 3) {
-    computerPick = "Scissors";
+    computerPick = rock;
+  } 
+  if (randomPick === 2) {
+    computerPick = paper;
+  } 
+  if (randomPick === 3) {
+    computerPick = scissors;
   }
 
   // Game logic based on the players pick
   if (playerPick === "") {
     return "Error";
-  } else if (computerPick === playerPick) {
-    return { result: "It's a tie!!", cp: computerPick, pp: playerPick };
-  } else if (computerPick === "Rock" && playerPick === "Paper") {
-    return { result: "You win!!", cp: computerPick, pp: playerPick };
-  } else if (computerPick === "Rock" && playerPick === "Scissors") {
-    return { result: "You lose!!", cp: computerPick, pp: playerPick };
-  } else if (computerPick === "Paper" && playerPick === "Scissors") {
-    return { result: "You win!!", cp: computerPick, pp: playerPick };
-  } else if (computerPick === "Paper" && playerPick === "Rock") {
-    return { result: "You lose!!", cp: computerPick, pp: playerPick };
-  } else if (computerPick === "Scissors" && playerPick === "Paper") {
-    return { result: "You lose!!", cp: computerPick, pp: playerPick };
-  } else if (computerPick === "Scissors" && playerPick === "Rock") {
-    return { result: "You win!!", cp: computerPick, pp: playerPick };
   }
+  if (computerPick === playerPick) {
+    result = "It's a tie!!";
+  }
+  if (computerPick === rock && playerPick === paper) {
+    result = win;
+  }
+  if (computerPick === rock && playerPick === scissors) {
+    result = lose;
+  }
+  if (computerPick === paper && playerPick === rock) {
+    result = lose;
+  }
+  if (computerPick === paper && playerPick === scissors) {
+    result = win;
+  }
+  if (computerPick === scissors && playerPick === paper) {
+    result = lose;
+  }
+  if (computerPick === scissors && playerPick === rock) {
+    result = win;
+  }
+  return { result: result, cp: computerPick, pp: playerPick };
 };
